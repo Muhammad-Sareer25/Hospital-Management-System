07c4aca8d15f6361744854b538b5e79c3d4dee09
Muhammad-Sareer25 <sareer2002@gmail.com>
2025-06-09 23:42:41 +0500

    2nd Commit by Sareer

--- src/main/java/com/example/hms/HelloApplication.java
+++ src/main/java/com/example/hms/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.hms;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,19 +12,21 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MAIN11.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("AdminMainForm.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
 
 
 
 
         stage.setTitle("Hospital Management System");
         stage.setScene(scene);
-        stage.setFullScreen(true);  // full screen view
+     //   stage.setFullScreen(true);  // full screen view
         stage.show();
     }
 
     public static void main(String[] args) {
+
         launch();
     }
 }
